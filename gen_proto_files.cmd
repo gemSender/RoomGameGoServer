b85a1e845baea9660d689b4d10d8fd355022eba1
xuehan <distmake>
2016-04-06 16:09:43 +0800

    refactor

--- gen_proto_files.cmd
+++ gen_proto_files.cmd
@@ -1,2 +1,3 @@
-protoc --go_out=src/messages/room_messages proto_files/Messages.proto
+protoc --go_out=src/messages/ proto_files/Messages.proto
+protoc --go_out=src/messages/room_messages proto_files/RoomMessage.proto
 protoc --go_out=src/messages/world_messages proto_files/WorldMessage.proto
--- gen_proto_files.sh
+++ gen_proto_files.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-protoc --go_out=src/messages proto_files/*.proto
+protoc --go_out=src/messages/ proto_files/Messages.proto
+protoc --go_out=src/messages/room_messages proto_files/RoomMessage.proto
+protoc --go_out=src/messages/world_messages proto_files/WorldMessage.proto
